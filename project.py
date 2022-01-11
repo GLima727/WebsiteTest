4cd5c152754bdb4f1e1eef28a01da6eb01e71ea4
GLima727 <guilhermegutierrez727@gmail.com>
2022-01-11 22:47:00 +0000

    Manga update tokyo revengas

---
+++ project.py
@@ -0,0 +1,100 @@
+from bs4 import BeautifulSoup
+import requests
+import cgitb 
+cgitb.enable()
+
+page = requests.get("https://tokyorevengersmanga.com/")
+
+html_doc = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+#print(soup.prettify())
+# <html>
+#  <head>
+#   <title>
+#    The Dormouse's story
+#   </title>
+#  </head>
+#  <body>
+#   <p class="title">
+#    <b>
+#     The Dormouse's story
+#    </b>
+#   </p>
+#   <p class="story">
+#    Once upon a time there were three little sisters; and their names were
+#    <a class="sister" href="http://example.com/elsie" id="link1">
+#     Elsie
+#    </a>
+#    ,
+#    <a class="sister" href="http://example.com/lacie" id="link2">
+#     Lacie
+#    </a>
+#    and
+#    <a class="sister" href="http://example.com/tillie" id="link3">
+#     Tillie
+#    </a>
+#    ; and they lived at the bottom of a well.
+#   </p>
+#   <p class="story">
+#    ...
+#   </p>
+#  </body>
+# </html>
+
+soup.title
+# <title>The Dormouse's story</title>
+
+soup.title.name
+# u'title'
+
+soup.title.string
+# u'The Dormouse's story'
+
+soup.title.parent.name
+# u'head'
+
+soup.p
+# <p class="title"><b>The Dormouse's story</b></p>
+
+soup.p['class']
+# u'title'
+
+soup.li
+# <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>
+
+soup.find_all("a")
+# [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,
+#  <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>,
+#  <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>]
+
+d = str(soup.find(class_= "su-posts su-posts-list-loop"))
+list = d.split("\n")
+
+
+f = open("lastchapter.txt", "r")
+
+if(f.read() != list[3]):
+    print("new chapter\n")
+    f = open("lastchapter.txt", "w")
+    f.write(list[3])
+else:
+    print("no new chapter :(")
+
+f.close()
+#print(d.split("\n"))
+# <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>
+
+
