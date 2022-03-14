2ca40ac61a5134c50a82b2ef369aa100e008a608
Guilherme Lima <82712415+GLima727@users.noreply.github.com>
2022-03-14 23:16:51 +0000

    Pokemon site with player name set button

---
+++ code.js
@@ -0,0 +1,91 @@
+
+let POKEMON_LIMIT_ERROR_MSG = "Too many pokemons bruh, we're runnin a business ovah here have some sense"
+let POKEMON_LIMIT = 6
+
+class Player {
+    constructor() {
+        this.name = ""
+        this.age = 0
+        this.numPokemons = 0
+        this.pokemons = []
+        this.bag = new Bag()
+
+    }
+    addPokemon(pokemon){
+        
+        try{
+            if(this.numPokemons == POKEMON_LIMIT){ //checks if the pokemon limit has been reached
+                throw tooManyPokemonsException
+            }
+
+            this.pokemons.push(pokemon) 
+            this.numPokemons++
+
+        }catch(pokemonLimitException){
+            document.writeln(POKEMON_LIMIT_ERROR_MSG)
+        }
+    }
+
+}
+
+class Pokemon {
+    constructor(name, type, level) {
+
+        this.name = name
+        this.type = type
+        this.level = level
+        this.xp = 0
+    }
+
+}
+
+class Pikachu extends Pokemon{
+    constructor(name, level){
+
+        super(name, "Lightning", level)
+        
+    }
+}
+
+class Bag {
+    constructor() {
+
+        this.numItems = 0
+        this.isFull = false
+        this.items = []
+
+    }
+
+}
+
+class Potion{
+    constructor(quantity, tag){
+        this.quantity = quantity
+        this.tag = tag
+    }
+}
+
+function gameStart() {
+    playerStart()
+}
+
+function playerStart(){
+
+}
+function receivePlayerName(){
+    gameStart();
+    var playerName = document.getElementById("nameInput").value;
+    if(playerName != ""){
+        player.name = playerName;
+        
+        document.getElementById("menuText").innerHTML = "<p>Player Name: " + player.name + "</p>";
+
+        document.getElementById("button1").style.display = "none"; /*deletes the buttons */
+        document.getElementById("nameInput").style.display = "none";
+
+        document.getElementById("gameText").innerHTML = "<l> WELCOME TO THE GAME <span style='color: rgb(30, 188, 216);font-family: cursive'>" 
+                                                        + playerName +"</span>!<br/> YOU THINK YOU TOUGH?<l/><p> WELL YA AINT </p>"
+    }
+}
+const player = new Player();
+
