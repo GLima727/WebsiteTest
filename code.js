5079dfcf6b610ec269ebd58a7d37fc6c024e8ee3
GLima727 <guilhermegutierrez727@gmail.com>
2022-03-17 22:58:36 +0000

    omg

--- code.js
+++ code.js
@@ -75,33 +75,16 @@ function playerStart(){
 function receivePlayerName(){
     gameStart();
     var playerName = document.getElementById("nameInput").value;
-    console.log(playerName.length)
-
-    if(playerName.length > 10){
-
-        document.getElementById("nameInputError").innerHTML = "<l> The name must have 10 characters or less.<l/>";
-    }
-    else if(playerName == "Joe" || playerName == "joe"){
-
-        document.getElementById("nameInputError").innerHTML = "<l> Joe who?<l/>";
-    }
-    else if(playerName == "Joe Mamma" || playerName == "Joe mamma" || playerName == "joe Mamma" || playerName == "joe mamma"){
-
-        document.getElementById("nameInputError").innerHTML = "<l> Joe Mamma who?<l/>";
-    }
-
-    else if(playerName != ""){
+    if(playerName != ""){
         player.name = playerName;
         
-        document.getElementById("headerMenuText").innerHTML = "<p>Player Name: " + player.name + "</p>";
+        document.getElementById("menuText").innerHTML = "<p>Player Name: " + player.name + "</p>";
 
-        /*document.getElementById("button1").style.display = "none"; deletes the buttons 
-        document.getElementById("nameInput").style.display = "none";*/
-        document.getElementById("button1").style.animation = document.getElementById("nameInput").style.animation = "fadeOut 700ms forwards";
-        document.getElementById("nameInputError").style.display = "none";
+        document.getElementById("button1").style.display = "none"; /*deletes the buttons */
+        document.getElementById("nameInput").style.display = "none";
 
-        document.getElementById("startMenu").innerHTML = "<l> Welcome <span style='color: #8d99ae;text-decoration:underline;font-weight:bold'>" 
-                                                        + playerName +"</span>!<br/> Please choose your starter.<l/>"
+        document.getElementById("gameText").innerHTML = "<l> WELCOME TO THE GAME <span style='color: rgb(30, 188, 216);font-family: cursive'>" 
+                                                        + playerName +"</span>!<br/> YOU THINK YOU TOUGH?<l/><p> WELL YA AINT </p>"
     }
 }
 const player = new Player();
